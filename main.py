c4ecbf1528ec36d8d985f2e141c38d7581681d99
ray lee <2lee.raymond@gmail.com>
2023-07-24 01:38:23 -0700

    Create basic summarization refine chain

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python3
-from utils.parse import generate_major_docs
+import os
+from utils.pinecone import configure_environment
+from utils.evaluations import fetch_json, data_to_str_list
+from CustomSplitter import CustomSplitter
+
+from langchain import OpenAI
+from langchain.chains.summarize import load_summarize_chain
 
 
 def main():
-    major = "computerscience"
+    configure_environment()
 
-    # Generate a list of Document's to upload to Pinecone index
-    docs = generate_major_docs(major)
+    eval_json_path = os.path.join(
+        os.getcwd(), "instructors/timng/CMSC_28000-Spring_2023.json"
+    )
 
-    print(docs[36])
+    data = fetch_json(eval_json_path)
+    str_list = data_to_str_list(data)
+
+    text_splitter = CustomSplitter()
+    docs = text_splitter.create_documents(str_list)
+
+    llm = OpenAI()
+
+    chain = load_summarize_chain(llm, chain_type="refine", verbose=True)
+    final_response = chain.run(docs)
+
+    print(final_response)
 
 
 if __name__ == "__main__":
